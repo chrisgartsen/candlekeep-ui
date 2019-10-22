4c305ebeb0ef312dba84e1440180f17318012c97
Chris Gartsen <chris@gartsen.nl>
2019-10-22 16:59:24 +0200

    Add validation for login form

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -8,8 +8,17 @@
         </q-card-section>
         <q-card-section>
           <q-form class="q-gutter-md" @submit.prevent="submitForm" > 
-            <q-input outlined label="Email" v-model="email" />
-            <q-input type="password" outlined label="Password" v-model="password" />
+            <q-input outlined label="Email" 
+                     v-model="email" 
+                     @blur="$v.email.$touch" 
+                     :error="$v.email.$error" 
+                     :error-message="emailError" />
+            <q-input outlined label="Password" 
+                     type="password"
+                     v-model="password" 
+                     @blur="$v.password.$touch"
+                     :error="$v.password.$error"
+                     :error-message="passwordError" />
           </q-form>
         </q-card-section> 
         <q-card-actions align="right">
@@ -24,6 +33,7 @@
 
 <script>
 import { mapActions } from 'vuex'
+import { required, minLength, email } from 'vuelidate/lib/validators'
 
 export default {
   name: 'layout',
@@ -33,10 +43,35 @@ export default {
       password: ''
     }
   },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
+  },
+  computed: {
+    emailError() {
+      if(!this.$v.email.required) return "Field is required"
+      if(!this.$v.email.email) return "Must be a valid email"
+      return ''
+    },
+    passwordError() {
+      if(!this.$v.password.required) return "Field is required"
+      if(!this.$v.password.minLength) return "Minimum length is 6 characters"
+      return ''
+    }
+  },
   methods: {
     ...mapActions('auth', ['login']),
     submitForm() {
-      this.login({ email: this.email, password: this.password })
+      this.$v.$touch()
+      if(!this.$v.$error) {
+        this.login({ email: this.email, password: this.password })
+      }
     }
   }
 }
