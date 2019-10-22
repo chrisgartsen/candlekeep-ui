7666efd922ad7e6956fe3a52ec0c9b14442bf8f3
Chris Gartsen <chris@gartsen.nl>
2019-10-22 14:26:38 +0200

    Show drawer on resize

--- src/layouts/page-drawer.vue
+++ src/layouts/page-drawer.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-drawer v-model="showDrawer" bordered content-class="bg-grey-2">
+    <q-drawer v-model="show" bordered content-class="bg-grey-2">
       <q-list>
         <q-item-label header>Actions</q-item-label>
         <q-item clickable to="/books">
@@ -15,13 +15,24 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'page-drawer',
 
   computed: {
     ...mapGetters('layout',['showDrawer']),
+    show: {
+      get() {
+        return this.showDrawer
+      },
+      set(newVal) {
+        this.toggleDrawer()
+      }
+    }
+  },
+  methods: {
+    ...mapActions('layout', ['toggleDrawer'])
   }
 
 }
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page>
-    Login
+
+    <div class="row">
+      <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">.col-md-6 .offset-md-3</div>
+    </div>
+
   </q-page>
 </template>
 
@@ -9,3 +13,16 @@ export default {
   name: 'layout'
 }
 </script>
+
+<style lang="sass" scoped>
+.row
+  background: rgba(255,0,0,.1)
+.row > div
+  padding: 10px 15px
+  background: rgba(86,61,124,.15)
+  border: 1px solid rgba(86,61,124,.2)
+.row + .row
+  margin-top: 1rem
+.medium
+  min-width: 400px
+</style>
