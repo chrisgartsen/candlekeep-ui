e295466a2120240aa669d90c51745c3b65ca5f4e
Chris Gartsen <chris@gartsen.nl>
2019-11-21 15:13:11 +0100

    Add authors index page

--- src/pages/authors/index.vue
+++ src/pages/authors/index.vue
@@ -5,12 +5,24 @@
       <q-breadcrumbs-el label="Authors" />
     </q-breadcrumbs>
 
-    <br /><br /><br /><br />
-    Authors: {{ authors }}
-
-    <br /><br /><br /><br /><br /><br />
-
-    <q-btn label="Add author" color="primary" @click="showDialog" />
+    <div class="q-mt-xl q-mb-xl row">
+      <q-table class="col-4 offset-4" title="Authors" :data="authors" :columns="columns" row-key="_id" :pagination.sync="pagination">
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td key="name" :props="props">{{ props.row.name }} </q-td>
+            <q-td key="actions" :props="props">
+              <q-btn flat color="primary" icon="edit" />              
+              <q-btn flat color="primary" icon="delete"/>
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </div>
+    <div class="row">
+      <div class="col offset-4">
+        <q-btn label="Add author" color="primary" @click="showDialog" />
+      </div>
+    </div>
 
     <q-dialog v-model="dialog">
       <q-card style="width:400px; max-width: 60vw;">
@@ -49,7 +61,7 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 import { required } from "vuelidate/lib/validators";
 
 export default {
@@ -57,7 +69,14 @@ export default {
   data() {
     return {
       dialog: false,
-      name: ""
+      name: "",
+      pagination: {
+        rowsPerPage: 30
+      },
+      columns: [     
+        { name: 'name', label: 'Name', field: 'name', align: 'left' },
+        { name: 'actions', label: '' }
+      ]
     };
   },
   validations: {
@@ -67,17 +86,20 @@ export default {
   },
   computed: {
     ...mapGetters("authors", ["authors"]),
+
     nameError() {
       if(!this.$v.name.required) return "Field is required"
     }
   },
   methods: {
+    ...mapActions('authors', ['create', 'fetchAll']),
     showDialog() {
       this.dialog = true;
     },
     submitForm() {
       this.$v.$touch()
       if(!this.$v.$error) {
+        this.create({ name: this.name })
         this.dialog = false;
       }
     },
@@ -85,6 +107,9 @@ export default {
       this.name = ''
       this.$v.$reset()
     }
+  },
+  created() {
+    this.fetchAll()
   },
   watch: {
     dialog(newVal) {
--- src/store/authors.js
+++ src/store/authors.js
@@ -1,3 +1,5 @@
+import Axios from  'axios'
+
 export default {
   namespaced: true,
 
@@ -8,6 +10,36 @@ export default {
   getters: {
     authors(state) {
       return state.authors
+    }
+  },
+
+  mutations: {
+    add(state, author) {
+      state.authors.push(author)
+    },
+    setAll(state, authors) {
+      state.authors = authors
+    }
+  },
+
+  actions: {
+    async fetchAll({ commit }) {
+      try {
+        const response = await Axios.get('/api/authors')
+        commit('setAll', response.data.authors)
+      } catch (err) {
+        throw err
+      }
+    },
+
+    async create({ commit, rootGetters }, payload) {
+      const userId = rootGetters['auth/userId']
+      try {
+        const response = await Axios.post('/api/authors', { name: payload.name, user: userId })
+        commit('add', response.data)
+      } catch (err) {
+        throw err
+      }
     }
   }
 
