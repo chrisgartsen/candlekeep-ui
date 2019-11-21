33b6b88c30364b29051c58216f8594f8619c0dfb
Chris Gartsen <chris@gartsen.nl>
2019-11-21 14:01:25 +0100

    Autlogin check JWT token validity

--- src/boot/auto-login.js
+++ src/boot/auto-login.js
@@ -1,10 +1,17 @@
 import { LocalStorage } from 'quasar'
+import Axios from 'axios'
 
 export default async ({ store }) => {
   const token = LocalStorage.getItem('webToken')
   const userId = LocalStorage.getItem('userId')
 
   if(token && userId) {
+    // Verify token
+    const response = await Axios.get('http://localhost:3000/auth/verify_credentials', {
+      headers: {
+        'x-auth-token': token
+      }
+    })
     store.dispatch('auth/autoLogin', { token: token, userId: userId })
   } else {
     store.dispatch('auth/logout')
--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,6 +4,7 @@ export default ({ Vue, store, router }) => {
 
   // Set the web token in the request if it exists
   axios.interceptors.request.use((config) => {
+    console.log("Intercepting")
     config.baseURL = 'http://localhost:3000'
     const token = store.getters['auth/token']
     if(token) config.headers = { 'x-auth-token': token }
