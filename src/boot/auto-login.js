69ddd83014034b5be437f6363ba6b33217a3d47f
Chris Gartsen <chris@gartsen.nl>
2019-11-26 21:52:05 +0100

    Logout on invalid autologin

--- src/boot/auto-login.js
+++ src/boot/auto-login.js
@@ -1,18 +1,19 @@
 import { LocalStorage } from 'quasar'
 import Axios from 'axios'
 
-export default async ({ store }) => {
+export default async ({ store, router }) => {
   const token = LocalStorage.getItem('webToken')
   const userId = LocalStorage.getItem('userId')
 
   if(token && userId) {
     // Verify token
-    const response = await Axios.get('http://localhost:3000/auth/verify_credentials', {
-      headers: {
-        'x-auth-token': token
-      }
-    })
-    store.dispatch('auth/autoLogin', { token: token, userId: userId })
+    const response = await Axios.get('http://localhost:3000/auth/verify_credentials', { headers: { 'x-auth-token': token } })
+    if(!response) { 
+      store.dispatch('auth/logout')
+      router.push('/login')
+    } else {
+      store.dispatch('auth/autoLogin', { token: token, userId: userId })
+    }
   } else {
     store.dispatch('auth/logout')
   }
